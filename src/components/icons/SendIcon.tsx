048f98fe6ca332ee4f36e261321ee9ade8b0fa1c
Nico <nico_cartellone@hotmail.com>
2024-01-10 20:46:16 -0300

    fix: icons props

--- src/components/icons/SendIcon.tsx
+++ src/components/icons/SendIcon.tsx
@@ -1,15 +1,19 @@
 const SendIcon = () => {
-        return(
-            <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33 33">
-                <g>
-                    <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
-                        <g fill="#fff" transform="translate(-568 -254)">
-                            <path d="M584 283s-3.128-6.024-4-8l16.075-15.221L584 283zm-12-13l23-11-16 15c-.004-.004-7-4-7-4zm27-15c-1.156.563-31 15-31 15s10.052 6.059 10 6c-.017-.019 6 11 6 11s15.75-30.5 16-31c.219-.625-.25-1.312-1-1z"></path>
-                        </g>
-                    </g>
-                </g>
-            </svg>
-        )
-    }
+  return (
+    <svg
+      className="w-5 h-5"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 33 33"
+    >
+      <g>
+        <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+          <g fill="#fff" transform="translate(-568 -254)">
+            <path d="M584 283s-3.128-6.024-4-8l16.075-15.221L584 283zm-12-13l23-11-16 15c-.004-.004-7-4-7-4zm27-15c-1.156.563-31 15-31 15s10.052 6.059 10 6c-.017-.019 6 11 6 11s15.75-30.5 16-31c.219-.625-.25-1.312-1-1z"></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
 
-export default SendIcon
+export default SendIcon;
